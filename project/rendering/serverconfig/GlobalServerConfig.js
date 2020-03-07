672ac6ec0be3fe6c763480ff2d194123957e64ea
liaojianfeng <liaojianfeng@g7.com.cn>
2020-03-07 10:28:38 +0800

    删除了无用的注释

--- project/rendering/serverconfig/GlobalServerConfig.js
+++ project/rendering/serverconfig/GlobalServerConfig.js
@@ -1,5 +1,4 @@
 /*服务端地址*/
 const SERVER_ADDRESS = "http://localhost:17666/";
-//const SERVER_ADDRESS = "http://172.22.33.220:17666/";
 /*服务端中获取tab列表信息的接口地址*/
 const GET_TABINFO_LIST = SERVER_ADDRESS + "tabIndex/getTabinfoList";
